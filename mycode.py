4dda63847d99386f1f9e364a475ccd27a66d4564
10orabh <123290989+10orabh@users.noreply.github.com>
2026-02-10 11:41:17 +0530

    make new data file throgh code

---
+++ mycode.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import os
+
+# 1️⃣ Sample data
+data = {
+    "id": [1, 2, 3, 4],
+    "name": ["Aman", "Riya", "Rahul", "Neha"],
+    "age": [23, 25, 22, 24],
+    "city": ["Indore", "Bhopal", "Delhi", "Pune"]
+}
+
+# 2️⃣ Create DataFrame
+df = pd.DataFrame(data)
+
+
+# 4️⃣ Create data folder path
+data_dir = "data"
+
+# 5️⃣ Create data folder if not exists
+os.makedirs(data_dir, exist_ok=True)
+
+# 6️⃣ Final file path
+file_path = os.path.join(data_dir, "sampledata.csv")
+# 7️⃣ Save CSV inside data folder
+df.to_csv(file_path, index=False)
+
+print("✅ CSV file saved successfully!")
+print("📁 Location:", file_path)
